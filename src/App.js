dbe31389dc3e2d3c55bb1d36586dd624f6a15d91
Sudesh Sudeera <sudeshsudeera@gmail.com>
2024-01-31 19:08:14 +0530

    Optimize the code

--- src/App.js
+++ src/App.js
@@ -1,46 +1,66 @@
 import './App.css'
 
-const user1 = {
-  avatar:'https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p9263607_b_v9_aa.jpg',
-  name:'Sudesh Sudeera',
-  designation:'Computer Engineer',
-  salary:'95000'
-}
+const users = [
+  {
+    avatar:'https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p9263607_b_v9_aa.jpg',
+    name:'Sudesh Sudeera',
+    designation:'Computer Engineer',
+    salary:'95000',
+    code: 1
+  },
+  {
+    avatar:'https://people.com/thmb/xE9UNogGcWjaA7Zkj-nWfEz6Vtk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(689x409:691x411)/Ezra-Miller-as-Flash-Warner-Bros-080422-1a1d7a5357a84f5e8fe25fc4833afeef.jpg',
+    name:'Sudeera Bandara',
+    designation:'Telecommuniction Engineer',
+    salary:'85000',
+    code: 2
+  },
+  {
+    avatar:'https://deadline.com/wp-content/uploads/2022/10/creed-iii-michael-b-jordan-canelo-alvarez.jpg?w=1000',
+    name:'Bandara Konara',
+    designation:'Electrical Engineer',
+    salary:'75000',
+    code: 3
+  },
+  {
+    avatar:'https://m.media-amazon.com/images/M/MV5BMTI5ODY5NTUzMF5BMl5BanBnXkFtZTcwOTAzNTIzMw@@._V1_.jpg',
+    name:'Konara Sudeera',
+    designation:'Electronic Engineer',
+    salary:'65000',
+    code: 4
+  }
+];
 
-const user2 = {
-  avatar:'https://people.com/thmb/xE9UNogGcWjaA7Zkj-nWfEz6Vtk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(689x409:691x411)/Ezra-Miller-as-Flash-Warner-Bros-080422-1a1d7a5357a84f5e8fe25fc4833afeef.jpg',
-  name:'Sudeera Bandara',
-  designation:'Telecommuniction Engineer',
-  salary:'85000'
-}
-
-const user3 = {
-  avatar:'https://deadline.com/wp-content/uploads/2022/10/creed-iii-michael-b-jordan-canelo-alvarez.jpg?w=1000',
-  name:'Bandara Konara',
-  designation:'Electrical Engineer',
-  salary:'75000'
-}
-
-const user4 = {
-  avatar:'https://m.media-amazon.com/images/M/MV5BMTI5ODY5NTUzMF5BMl5BanBnXkFtZTcwOTAzNTIzMw@@._V1_.jpg',
-  name:'Konara Sudeera',
-  designation:'Electronic Engineer',
-  salary:'65000'
-}
 
 function UserManager() {
   return(
     <div className='wrapper'>
-      <User avatar={user1.avatar}
-        name={user1.name} designation={user1.designation} salary={user1.salary} />
-      <User avatar={user2.avatar}
-        name={user2.name} designation={user2.designation} salary={user2.salary} />
-      <User avatar={user3.avatar}
-        name={user3.name} designation={user3.designation} salary={user3.salary} />
-      <User avatar={user4.avatar}
-        name={user4.name} designation={user4.designation} salary={user4.salary} />
+      <SearchForm />
+      {users.map((user) => {
+        return <User {...user} key={user.code} />
+      })}
     </div>
   );
+}
+
+const SearchForm = ()=> {
+
+    return(
+      <div className='search-outer'>
+        <form onSubmit={(e)=> {
+          e.preventDefault();
+        }}>
+
+          <input type="search" name="searchText" onChange={(e)=> {
+            console.log(e.target.value);
+          }} /> | 
+          <button type='submit' onClick={(e)=> {
+            console.log('manageClickEvent');
+          }} >Search Now</button>
+
+        </form>
+      </div>
+    );
 }
 
 const User = (props)=> {
@@ -75,13 +95,19 @@ const UserData = ({name, designation, salary, children})=> {
       <p>
         {children}
       </p>
+
+      <button style={{cursor: 'pointer'}} onClick={(e)=> {
+        console.log(name);
+      }}>Print Data</button>
+
     </div>
   )
 }
 
 const UserName = ({name})=> {
   const inlineStyles = {
-    color: 'lightblue'
+    color: 'lightblue',
+    margin: 0
   }
 
   return(
@@ -94,7 +120,7 @@ const UserName = ({name})=> {
 const Designation = ({designation})=> {
   return(
     <div>
-      <h3 style={{fontStyle: 'italic'}}>{designation}</h3>
+      <h3 style={{fontStyle: 'italic', margin: 0}}>{designation}</h3>
     </div>
   )
 }
@@ -102,7 +128,7 @@ const Designation = ({designation})=> {
 const Salary = ({salary})=> {
   return(
     <div>
-      <h3 style={{fontStyle: 'italic'}}>{salary}</h3>
+      <h3 style={{fontStyle: 'italic', margin: 0}}>{salary}</h3>
     </div>
   )
 }
